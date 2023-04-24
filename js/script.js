e894e43cbe0dcc33dd7cea240b3ac46a5a7001d6
Xavier Biscay <xbdevweb@gmail.com>
2023-04-24 14:26:09 +0200

    xavier6

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-const 
+
+
+function getValue() {
+    // Sélectionner l'élément input et récupérer sa valeur
+    var input = document.querySelector(".pseudo").value;
+    // Afficher la valeur
+    localStorage.pseudo = input ;
+    document.location.href="mael2.html"; 
+}
+
+const valider = document.querySelector("#btn")
+
+valider.addEventListener("click",getValue)
