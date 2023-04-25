801e934bcd95b8d6097eb8595aff42df4cb1239f
Xavier Biscay <xbdevweb@gmail.com>
2023-04-25 14:11:26 +0200

    xavier12

--- js/fetch.js
+++ js/fetch.js
@@ -1,1 +1,11 @@
-fetch ()
+fetch ("https://quiz.freefakeapi.io/api/categories?limit=4&random=true",{
+    headers: {
+        "Accept-Language":"fr_FR"
+    }
+})
+.then (reponse => reponse.json())
+.then (theme => {
+    
+});
+
+
