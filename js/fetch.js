ec83c4718eccf7ce16794b574395afb51e001a05
Chosess <n.noelle552@gmail.com>
2023-04-25 14:58:11 +0200

    premiere réussite ouais

--- js/fetch.js
+++ js/fetch.js
@@ -1,11 +1,41 @@
-fetch ("https://quiz.freefakeapi.io/api/categories?limit=4&random=true",{
+fetch("https://quiz.freefakeapi.io/api/categories?limit=4&random=true", {
     headers: {
-        "Accept-Language":"fr_FR"
+        "Accept-Language": "fr_FR"
     }
 })
-.then (reponse => reponse.json())
-.then (theme => {
-    
-});
+    .then(reponse => reponse.json())
+    .then(theme => {
+        document.querySelector(".c1").textContent = theme[0].name
+        document.querySelector(".c2").textContent = theme[1].name
+        document.querySelector(".c3").textContent = theme[2].name
+        document.querySelector(".c4").textContent = theme[3].name
+
+        let c1 = document.querySelector(".c1")
+        let c2 = document.querySelector(".c2")
+        let c3 = document.querySelector(".c3")
+        let c4 = document.querySelector(".c4")
+
+        c1.addEventListener("click", function () {
+            sessionStorage.id = theme[0].id
+            document.location.href="Questions.html";
+        })
+
+        c2.addEventListener("click", function () {
+            sessionStorage.id = theme[1].id
+            document.location.href="Questions.html";
+        })
+
+        c3.addEventListener("click", function () {
+            sessionStorage.id = theme[2].id
+            document.location.href="Questions.html";
+        })
+
+        c4.addEventListener("click", function () {
+            sessionStorage.id = theme[3].id
+            document.location.href="Questions.html";
+        })
+
+        
+    });
 
 
