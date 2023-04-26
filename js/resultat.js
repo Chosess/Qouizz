6ccc21913c61c2c8ce89d22119963270a13b7978
Chosess <n.noelle552@gmail.com>
2023-04-26 11:24:03 +0200

    la suite

--- js/resultat.js
+++ js/resultat.js
@@ -2,4 +2,9 @@ document.querySelector(".scoreactuel").textContent = sessionStorage.score * 10
 
 // localStorage.scoretotal = localStorage.scoretotal + sessionStorage.score
 
-document.querySelector(".scoretotal").textContent = localStorage.scoretotal
+// document.querySelector(".scoretotal").textContent = localStorage.scoretotal
+
+
+document.querySelector(".qouizz").addEventListener("click", function() {
+    document.location.href="selection.html";
+})
