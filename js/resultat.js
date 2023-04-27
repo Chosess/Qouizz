d4ce4d97ebe3ec677ce17f322b61c5320f7a6ae1
Chosess <n.noelle552@gmail.com>
2023-04-27 13:55:19 +0200

    resultat

--- js/resultat.js
+++ js/resultat.js
@@ -1,13 +1,8 @@
 document.querySelector(".scoreactuel").textContent = sessionStorage.score * 10
 
-localStorage.scoretotal = localStorage.scoretotal + sessionStorage.score
+localStorage.scoretotal = Number(localStorage.scoretotal) + Number(sessionStorage.score)
 
 document.querySelector(".scoretotal").textContent = localStorage.scoretotal * 10
-
-
-
-
-
 
 document.querySelector(".qouizz").addEventListener("click", function() {
     document.location.href="selection.html";
