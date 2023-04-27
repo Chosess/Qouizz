6d40f6f2475e989ddfdbbe62ffc958941d426658
Chosess <n.noelle552@gmail.com>
2023-04-27 14:27:25 +0200

    voila

--- js/mauvaisereponse.js
+++ js/mauvaisereponse.js
@@ -1,5 +1,6 @@
 if(sessionStorage.numero == 10){
     document.querySelector(".lasuite").addEventListener("click", function(){
+        localStorage.scoretotal = Number(localStorage.scoretotal) + Number(sessionStorage.score)
         
     document.location.href="resultat.html"
     })
--- js/resultat.js
+++ js/resultat.js
@@ -1,7 +1,5 @@
 document.querySelector(".scoreactuel").textContent = sessionStorage.score * 10
 
-localStorage.scoretotal = Number(localStorage.scoretotal) + Number(sessionStorage.score)
-
 document.querySelector(".scoretotal").textContent = localStorage.scoretotal * 10
 
 localStorage.scorecumule++;
