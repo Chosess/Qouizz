4db8bd25139f3c31124160b702ee11096e297425
Chosess <n.noelle552@gmail.com>
2023-04-28 15:19:40 +0200

    fin4???

--- js/accueil.js
+++ js/accueil.js
@@ -4,17 +4,17 @@ function getValue() {
     var input = document.querySelector(".pseudo").value;
     localStorage.pseudo = input ;
     document.location.href="selection.html"; 
-}
+};
 
-const valider = document.querySelector("#btn")
+const valider = document.querySelector("#btn");
 
-valider.addEventListener("click",getValue)
+valider.addEventListener("click",getValue);
 
-const pseudoo = document.querySelector(".pseudo")
+const pseudoo = document.querySelector(".pseudo");
 
-let locals = localStorage.pseudo
+let locals = localStorage.pseudo || "";
 
-pseudoo.value = locals
+pseudoo.value = locals;
 
 // if(![localStorage.scoretotal >= 0]){
 //     localStorage.scoretotal = 0
